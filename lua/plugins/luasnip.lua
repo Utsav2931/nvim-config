a862389960d3dede2063515ebc47e282e7460fb9
Utsav <upatel231a@gmail.com>
2024-01-03 18:42:15 +0530

    Added auto pairs

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -3,7 +3,7 @@ return {
 	-- follow latest release.
 	version = "v2.*",
 	event = { "BufReadPre", "BufNewFile" },
-	dependencies = { "rafamadriz/friendly-snippets" },
+	dependencies = { "rafamadriz/friendly-snippets", "saadparwaiz1/cmp_luasnip" },
 	opts = {},
 	config = function()
 		require("luasnip.loaders.from_vscode").lazy_load()
